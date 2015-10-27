f1f659fd7c732ef641743cd5baf7cb97a0c57987
Kevin Purrmann <kpurrmann@Kevins-MBP.fritz.box>
2015-10-27 15:28:05 +0100

    [TASK] Add default Test

---
+++ Tests/Unit/DefaultClassTest.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: kpurrmann
+ * Date: 27.10.15
+ * Time: 15:24
+ */
+
+namespace Pws\KesearchCategories\Tests;
+
+
+use TYPO3\CMS\Core\Tests\UnitTestCase;
+
+class DefaultClassTest extends UnitTestCase
+{
+
+    public function testDefault()
+    {
+        $this->assertTrue(true);
+    }
+
+}
