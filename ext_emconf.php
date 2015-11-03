0a973ad5789dccc1f0a924af90e3b5447a2b9f33
Kevin Purrmann <kpurrmann@kevins-mbp.speedport_w_723v_1_39_000>
2015-11-03 12:56:11 +0100

    Update version number to 1.0.0

--- ext_emconf.php
+++ ext_emconf.php
@@ -15,7 +15,7 @@ $EM_CONF[$_EXTKEY] = array(
     'modify_tables' => '',
     'clearCacheOnLoad' => 1,
     'lockType' => '',
-    'version' => '0.0.9',
+    'version' => '1.0.0',
     'constraints' =>
         array(
             'depends' =>
