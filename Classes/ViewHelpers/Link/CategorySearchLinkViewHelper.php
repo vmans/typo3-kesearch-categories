88ac2088962a99871f77d1e53a34102a723d68e4
Kevin Purrmann <kpurrmann@Kevins-MacBook-Pro.local>
2015-11-05 11:19:28 +0100

    Add possibility to link directly to filtered search

---
+++ Classes/ViewHelpers/Link/CategorySearchLinkViewHelper.php
@@ -0,0 +1,98 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: kpurrmann
+ * Date: 05.11.15
+ * Time: 09:41
+ */
+
+namespace Pws\KesearchCategories\ViewHelpers\Link;
+
+
+use Pws\KesearchCategories\Domain\Model\Category;
+use Pws\KesearchCategories\Domain\Model\Filter;
+use Pws\KesearchCategories\Domain\Repository\FilterRepository;
+use TYPO3\CMS\Fluid\ViewHelpers\Link\PageViewHelper;
+
+class CategorySearchLinkViewHelper extends PageViewHelper
+{
+
+    /**
+     * @var array
+     */
+    protected $additionalParams = array();
+
+    /**
+     * @var \Pws\KesearchCategories\Domain\Repository\FilterRepository
+     * @inject
+     */
+    protected $filterRepository;
+
+    /**
+     * @param null $pageUid
+     * @param array $additionalParams
+     * @param int $pageType
+     * @param bool|false $noCache
+     * @param bool|false $noCacheHash
+     * @param string $section
+     * @param bool|false $linkAccessRestrictedPages
+     * @param bool|false $absolute
+     * @param bool|false $addQueryString
+     * @param array $argumentsToBeExcludedFromQueryString
+     * @param null $addQueryStringMethod
+     * @param Category|null $category
+     * @return string
+     */
+    public function render(
+        $pageUid = null,
+        array $additionalParams = array(),
+        $pageType = 0,
+        $noCache = false,
+        $noCacheHash = false,
+        $section = '',
+        $linkAccessRestrictedPages = false,
+        $absolute = false,
+        $addQueryString = false,
+        array $argumentsToBeExcludedFromQueryString = array(),
+        $addQueryStringMethod = null,
+        Category $category = null
+    ) {
+
+        $this->additionalParams = $additionalParams;
+        /* @var $filter \Pws\KesearchCategories\Domain\Model\Filter */
+        if ($category && ($filter = $this->filterRepository->findOneByCategory($category))) {
+            $this->additionalParams['tx_kesearch_pi1']['filter'] = $this->setUpFilterQuery($category, $filter);
+        }
+
+        return parent::render($pageUid, $this->additionalParams, $pageType, $noCache, $noCacheHash, $section,
+            $linkAccessRestrictedPages, $absolute, $addQueryString, $argumentsToBeExcludedFromQueryString,
+            $addQueryStringMethod);
+    }
+
+    /**
+     * @param Category $category
+     * @param Filter $filter
+     * @return array
+     */
+    protected function setUpFilterQuery(Category $category, Filter $filter)
+    {
+        var_dump($filter->isMultiSelectFilter());
+        if ($filter->isMultiSelectFilter()) {
+            $param = array($category->getUid() => $category->getFilterOptionTag());
+        } else {
+            $param = $category->getFilterOptionTag();
+        }
+
+        return array($filter->getUid() => $param);
+    }
+
+    /**
+     * @return array
+     */
+    public function getAdditionalParams()
+    {
+        return $this->additionalParams;
+    }
+
+
+}
---
+++ Tests/Unit/ViewHelpers/Link/CategorySearchLinkViewHelperTest.php
@@ -0,0 +1,140 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: kpurrmann
+ * Date: 05.11.15
+ * Time: 09:42
+ */
+
+namespace Pws\KesearchCategories\Tests\ViewHelpers\Link;
+
+
+use Pws\KesearchCategories\Domain\Model\Category;
+use Pws\KesearchCategories\Domain\Model\Filter;
+use Pws\KesearchCategories\Domain\Repository\FilterRepository;
+use Pws\KesearchCategories\ViewHelpers\Link\CategorySearchLinkViewHelper;
+use TYPO3\CMS\Fluid\Core\Rendering\RenderingContext;
+use TYPO3\CMS\Fluid\Tests\Unit\ViewHelpers\ViewHelperBaseTestcase;
+
+class CategorySearchLinkViewHelperTest extends ViewHelperBaseTestcase
+{
+
+    /**
+     * @var CategorySearchLinkViewHelper
+     */
+    protected $viewHelper;
+
+    /**
+     * @var FilterRepository|\PHPUnit_Framework_MockObject_MockObject
+     */
+    protected $filterRepository;
+
+    /**
+     *
+     */
+    protected function setUp()
+    {
+        parent::setUp();
+
+        $this->viewHelper = $this->getAccessibleMock(
+            'Pws\KesearchCategories\ViewHelpers\Link\CategorySearchLinkViewHelper',
+            array('renderChildren')
+        );
+        $this->filterRepository = $this->getMock(
+            'Pws\KesearchCategories\Domain\Repository\FilterRepository',
+            array('findOneByCategory'),
+            array(),
+            '',
+            false
+        );
+        $this->inject($this->viewHelper, 'filterRepository', $this->filterRepository);
+
+        $this->injectDependenciesIntoViewHelper($this->viewHelper);
+        $this->viewHelper->initializeArguments();
+    }
+
+    /**
+     * @dataProvider linkDataProvider
+     * @test
+     * @param \PHPUnit_Framework_MockObject_MockObject $categoryMock
+     * @param \PHPUnit_Framework_MockObject_MockObject $filterMock
+     * @param $type
+     * @param $expected
+     */
+    public function testGetFilterByCategory(
+        \PHPUnit_Framework_MockObject_MockObject $categoryMock,
+        \PHPUnit_Framework_MockObject_MockObject $filterMock,
+        $type,
+        $expected
+    ) {
+        $filterMock->method('isMultiSelectFilter')->willReturn($type);
+
+        $this->filterRepository->expects($this->once())
+            ->method('findOneByCategory')
+            ->with($categoryMock)
+            ->willReturn($filterMock);
+
+        $this->viewHelper->render(1, array(), 0, false, false, '', false, false, false, array(), null,
+            $categoryMock);
+
+        $this->assertSame($expected, $this->viewHelper->getAdditionalParams());
+    }
+
+    /**
+     * @test
+     */
+    public function testViewHelperExtendsFromPageLinkViewHelper()
+    {
+        $this->assertInstanceOf('TYPO3\CMS\Fluid\ViewHelpers\Link\PageViewHelper', $this->viewHelper);
+    }
+
+    /**
+     * @return array
+     */
+    public function linkDataProvider()
+    {
+        $categoryMock = $this->getMock(
+            'Pws\KesearchCategories\Domain\Model\Category',
+            array('getUid', 'getFilterOptionTag')
+        );
+
+        $categoryMock->expects($this->any())->method('getUid')->willReturn(1);
+        $categoryMock->expects($this->any())->method('getFilterOptionTag')->willReturn('1test');
+
+        $filterMock = $this->getMock(
+            'Pws\KesearchCategories\Domain\Model\Filter',
+            array('getUid', 'isMultiSelectFilter')
+        );
+
+        $filterMock->expects($this->any())->method('getUid')->willReturn(3);
+
+        return array(
+            array(
+                $categoryMock,
+                $filterMock,
+                false,
+                array(
+                    'tx_kesearch_pi1' => array(
+                        'filter' => array(
+                            '3' => '1test'
+                        )
+                    )
+                ),
+            ),
+            array(
+                $categoryMock,
+                clone $filterMock,
+                true,
+                array(
+                    'tx_kesearch_pi1' => array(
+                        'filter' => array(
+                            '3' => array(
+                                '1' => '1test'
+                            )
+                        )
+                    )
+                ),
+            )
+        );
+    }
+}
