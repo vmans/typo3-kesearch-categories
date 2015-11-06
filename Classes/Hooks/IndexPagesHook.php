22d0f399ba286f7c73cde92e427e114ac7b8480d
Kevin Purrmann <kpurrmann@Kevins-MacBook-Pro.local>
2015-11-06 08:00:35 +0100

    Fix class comment

--- Classes/Hooks/IndexPagesHook.php
+++ Classes/Hooks/IndexPagesHook.php
@@ -1,9 +1,15 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: kpurrmann
- * Date: 29.10.15
- * Time: 12:25
+/*
+ * This file is part of the TYPO3 CMS project.
+ *
+ * It is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License, either version 2
+ * of the License, or any later version.
+ *
+ * For the full copyright and license information, please read the
+ * LICENSE.txt file that was distributed with this source code.
+ *
+ * The TYPO3 project - inspiring people to share!
  */
 
 namespace Pws\KesearchCategories\Hooks;
