982d552e3d6aac8726370b6701ba13a21bd6f78b
Kevin Purrmann <kpurrmann@Kevins-MacBook-Pro.local>
2015-11-06 08:43:35 +0100

    Registered hook for content indexing

--- ext_localconf.php
+++ ext_localconf.php
@@ -2,3 +2,4 @@
 
 $GLOBALS['TYPO3_CONF_VARS']['EXTCONF']['ke_search']['modifyFilters'][] = 'Pws\\KesearchCategories\\Hooks\\FilterOptionHook';
 $GLOBALS['TYPO3_CONF_VARS']['EXTCONF']['ke_search']['modifyPagesIndexEntry'][] = 'Pws\\KesearchCategories\\Hooks\\IndexPagesHook';
+$GLOBALS['TYPO3_CONF_VARS']['EXTCONF']['ke_search']['modifyContentIndexEntry'][] = 'Pws\\KesearchCategories\\Hooks\\IndexContentHook';
