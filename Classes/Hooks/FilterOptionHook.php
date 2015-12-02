b61e2aa6faee427a762e463992fb55fe7b5ef8c0
Kevin Purrmann <kpurrmann@kevins-mbp.speedport_w_723v_1_39_000>
2015-12-02 13:06:32 +0100

    Fix if filter has no options than unset whole filter

--- Classes/Hooks/FilterOptionHook.php
+++ Classes/Hooks/FilterOptionHook.php
@@ -56,6 +56,10 @@ class FilterOptionHook extends AbstractHook
                     }
                 }
 
+                if (!isset($filters[$key]['options'])) {
+                    unset($filters[$key]);
+                }
+
             }
         }
 
