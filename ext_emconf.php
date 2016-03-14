3ae52f556d5e5c4eaba68caff6a1dbe6926b0d9b
Kevin Purrmann <kevin@purrmann-websolutions.de>
2016-03-14 13:29:47 +0100

    Version bump

--- ext_emconf.php
+++ ext_emconf.php
@@ -15,7 +15,7 @@ $EM_CONF[$_EXTKEY] = array(
     'modify_tables' => '',
     'clearCacheOnLoad' => 1,
     'lockType' => '',
-    'version' => '1.1.1',
+    'version' => '1.1.2',
     'constraints' =>
         array(
             'depends' =>
